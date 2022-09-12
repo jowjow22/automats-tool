3f94bcef5d6c235b3e8ae655b310b6e8c50334dc
jowjow22 <jonathan224santos@gmail.com>
2022-09-11 22:32:29 -0300

    fix: change default index.html to work on both platforms

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import P5 from 'p5';
-import { useLayoutEffect, useState } from 'react';
 import { sketch } from './Classes/index'
 import { Frame } from './styles/App.styles'
 
-export let game: P5;
+export let game = new P5(sketch);
 
 
 
 function App() {
-  useLayoutEffect(()=>{
-    game = new P5(sketch);
-  }, [])
-
   return (
     <>
       <Frame id='AppFrame'>asdas</Frame>
--- src/styles/App.styles.ts
+++ src/styles/App.styles.ts
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 
 export const Frame = styled.div`
-  float: right;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  & > canvas {
+    border-radius: 10px;
+  }
 `
